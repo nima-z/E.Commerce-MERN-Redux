f1ad6beb3535fb35fc908b6970c4482657df78ed
nima-z <nima13zare@gmail.com>
2022-05-18 21:21:03 +0430

    admin panel added

---
+++ admin/src/App.js
@@ -0,0 +1,62 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+import Auth from "./pages/authentication/auth";
+import { Fragment } from "react";
+
+function App() {
+  const admin = JSON.parse(
+    JSON.parse(localStorage.getItem("persist:root")).user
+  ).currentUser.user.isAdmin;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        {admin ? (
+          <Fragment>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/users">
+                <UserList />
+              </Route>
+              <Route path="/user/:userId">
+                <User />
+              </Route>
+              <Route path="/newUser">
+                <NewUser />
+              </Route>
+              <Route path="/products">
+                <ProductList />
+              </Route>
+              <Route path="/product/:productId">
+                <Product />
+              </Route>
+              <Route path="/newproduct">
+                <NewProduct />
+              </Route>
+            </div>
+          </Fragment>
+        ) : (
+          <Auth />
+        )}
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ admin/src/pages/authentication/auth.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import styles from "./auth.module.css";
+import { useDispatch } from "react-redux";
+import { loggingIn } from "../../helpers/authMethod";
+
+export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  function emailHandler(e) {
+    setEmail(e.target.value);
+  }
+  function passwordHandler(e) {
+    setPassword(e.target.value);
+  }
+  function submitHandler(e) {
+    e.preventDefault();
+    loggingIn(dispatch, { email, password });
+  }
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={submitHandler} className={styles.form}>
+        <input type="text" placeholder="email" onChange={emailHandler} />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={passwordHandler}
+        />
+        <button>sign in</button>
+      </form>
+    </div>
+  );
+}
