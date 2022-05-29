0a4301a6b99c5975c031eb2770bfe1f3215b8744
nima-z <nima13zare@gmail.com>
2022-05-29 17:08:05 +0430

    upload image works fine

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -42,26 +42,6 @@ export default function FeaturedInfo() {
         </div>
         <span className="featuredSub">Compared to last month</span>
       </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Sales</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$4,415</span>
-          <span className="featuredMoneyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative" />
-          </span>
-        </div>
-        <span className="featuredSub">Compared to last month</span>
-      </div>
-      <div className="featuredItem">
-        <span className="featuredTitle">Cost</span>
-        <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,225</span>
-          <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon" />
-          </span>
-        </div>
-        <span className="featuredSub">Compared to last month</span>
-      </div>
     </div>
   );
 }
